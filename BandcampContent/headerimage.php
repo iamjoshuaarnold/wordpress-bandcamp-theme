f35e658e292ada7efed87be24a01562b4266323d
deadpan110 <deadpan110@77d3d834-ded4-11de-b4bf-3fb2a63344f9>
2009-12-12 08:24:48 +0000

    Removed test content and made theme adjustments to correctly use WordPress header and footer methods

--- BandcampContent/headerimage.php
+++ BandcampContent/headerimage.php
@@ -1,15 +1,12 @@
 <?php
 
-
-
-
 function bandcamp_unconfigured_headerimg() {
 	$headerImage = get_bloginfo('template_url') . "/images/WordPress-Bandcamp-Theme-975x86-png8-64color.png";
 	$headerLink = get_option('home');
 	$headerName = get_bloginfo('name');
 	$headerDesc = get_bloginfo('description');
-	echo <<<END
-		<div id="header">
+echo <<<END
+		<!--<div id="header">-->
 				<h1 class="hiddenAccess"><a href="$headerLink/">$headerName</a></h1>
 				<div class="hiddenAccess description">$headerDesc</div>
 			<div id="customHeaderWrapper">
@@ -17,7 +14,7 @@ function bandcamp_unconfigured_headerimg() {
 					<a href="$headerLink/"><img id="headerImage" src="$headerImage" /></a>
 				</div>
 			</div>
-		</div>
+		<!--</div>-->
 END;
 	
 }
