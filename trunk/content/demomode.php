e4d24c9ec69da0574618c06f56e38977c14afec1
deadpan110 <deadpan110@77d3d834-ded4-11de-b4bf-3fb2a63344f9>
2009-12-15 15:03:18 +0000

    Yet a few more sidebar style alterations in preparation for working demo code

--- trunk/content/demomode.php
+++ trunk/content/demomode.php
@@ -4,6 +4,7 @@ function bandcamp_demomode_leftside() {
 <li id="trydemo">
    <form id="domainform" method="post" action="<?php bloginfo('home'); ?>">
 	<div>
+		<h2>Try It Out Now!</h2>
 		<label for="BandcampDomain"><?php _e('Enter your Bandcamp domain:'); ?></label>
 		<input type="text" name="BandcampDomain" id="BandcampDomain" size="15" />
 		<br />
