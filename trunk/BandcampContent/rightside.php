2330a815af8438269bc7e091450245331787ae00
deadpan110 <deadpan110@77d3d834-ded4-11de-b4bf-3fb2a63344f9>
2009-12-17 10:15:39 +0000

    No Bandcamp domain name sidebar tweak for self promotion :P

--- trunk/BandcampContent/rightside.php
+++ trunk/BandcampContent/rightside.php
@@ -10,7 +10,27 @@ function bandcamp_rightside() {
 			$rightside = $bandcamp_dom->saveXML($html);
 		}
 	} else {
-		$rightside = 'Bandcamp Discography';
+		$googleImage = get_bloginfo('template_url') . "/images/WPBC-Google-code-100x100.jpg";
+
+$rightside = <<< EOF
+<div id="rightColumn" class="rightColumn">
+	<div id="discography">
+		<h3 class="title">Useful Links</h3>
+		<ul title="Get Involved at Google Code">
+			<li>
+				<div>
+					<div class="thumbthumb">
+						<a href="http://code.google.com/p/wordpress-bandcamp-theme/"><img class="thumbthumb" src="$googleImage" alt="Get Involved at Google Code"/></a>
+					</div>
+				</div>
+				<div class="trackTitle"><a href="http://code.google.com/p/wordpress-bandcamp-theme/">Submit code and report bugs</a></div>
+				<div class="trackYear secondaryText">Get Involved</div>
+			</li>
+		</ul>
+	</div>
+</div>
+EOF;
+
 	}
 	echo $rightside;
 }
