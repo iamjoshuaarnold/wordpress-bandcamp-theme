be6fec311097389a39d729da7f0f7f69f45a0bb0
deadpan110 <deadpan110@77d3d834-ded4-11de-b4bf-3fb2a63344f9>
2009-12-18 12:39:52 +0000

    Added the right sidebar images to include a link back to bandcamp and a bit of shameless advertising (note, these can be disabled if required and will not show within a configured theme - try the demo)

--- trunk/BandcampContent/rightside.php
+++ trunk/BandcampContent/rightside.php
@@ -11,7 +11,8 @@ function bandcamp_rightside() {
 		}
 	} else {
 		$googleImage = get_bloginfo('template_url') . "/images/WPBC-Google-code-100x100.jpg";
-
+		$bandcampImage = get_bloginfo('template_url') . "/images/Bandcamp-100x100.jpg";
+		$site4fansImage = get_bloginfo('template_url') . "/images/Site4FansBCtheme-promo-100x100.jpg";
 $rightside = <<< EOF
 <div id="rightColumn" class="rightColumn">
 	<div id="discography">
@@ -25,6 +26,24 @@ $rightside = <<< EOF
 				</div>
 				<div class="trackTitle"><a href="http://code.google.com/p/wordpress-bandcamp-theme/">Submit code and report bugs</a></div>
 				<div class="trackYear secondaryText">Get Involved</div>
+			</li>
+			<li>
+				<div>
+					<div class="thumbthumb">
+						<a href="http://bandcamp.com/"><img class="thumbthumb" src="$bandcampImage" alt="Behold! Bandcamp"/></a>
+					</div>
+				</div>
+				<div class="trackTitle"><a href="http://bandcamp.com/">The best home on the web for your band's music</a></div>
+				<div class="trackYear secondaryText">Sign up now</div>
+			</li>
+			<li>
+				<div>
+					<div class="thumbthumb">
+						<a href="http://site4fans.com/"><img class="thumbthumb" src="$site4fansImage" alt="The home of fansites"/></a>
+					</div>
+				</div>
+				<div class="trackTitle"><a href="http://site4fans.com.com/">Host your band's blog for FREE</a></div>
+				<div class="trackYear secondaryText">The home of fansites</div>
 			</li>
 		</ul>
 	</div>
